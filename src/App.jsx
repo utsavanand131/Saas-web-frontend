67a8eb38ff6fdfcb324b03e571b55891d41ee39d
utsavanand131 <utsavanand807@gmail.com>
2026-01-06 16:29:04 +0530

    Set up routing and app layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Navbar from "./components/layout/Navbar";
 import AppRoutes from "./routes/AppRoutes";
+import Footer from "./components/layout/Footer";
+
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-slate-200 to-slate-500 text-slate-900">
+    <div className="min-h-screen bg-gradient-to-br from-slate-200 to-slate-800 text-slate-900">
       <Navbar />
       <AppRoutes />
+      <Footer />
+
     </div>
   );
 }
